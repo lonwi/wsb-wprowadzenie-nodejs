7211e632c8eb92abe35e995c695f0641ea7350b7
Wojciech Bubolka <bubolka@gmail.com>
2019-01-13 10:35:44 +0000

    dodanie nodemon

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
 var express = require('express');
-
+var path = require('path');
 var PORT = 8080;
 
 var app = express();
 
-app.get('*', function(req, res) {
-    res.json({
-        hello: 'world'
-    });
-});
+// app.get('*', function(req, res) {
+//     res.json({
+//         hello: 'world'
+//     });
+// });
+
+app.get('*', express.static(path.resolve(__dirname, 'dist')));
 
 app.listen(PORT, function () {
     console.log('listening on port', PORT);
-    console.log('Wojtek Jestes Super Chłopak');
+    console.log('Wojtek Jestes Super Chłopak!');
 });
