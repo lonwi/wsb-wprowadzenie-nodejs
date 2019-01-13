0c70b592000dc041c252036890511b241301d96d
Wojciech Bubolka <bubolka@gmail.com>
2019-01-13 12:36:28 +0000

    wymiana danych - 1

--- src/main.js
+++ src/main.js
@@ -1,1 +1,20 @@
-console.log('Hello')
+document.addEventListener('DOMContentLoaded', function() {
+    const content = document.getElementById('content');
+    const form = document.getElementById('todos');
+    const input = document.getElementById('todo');
+    
+    function addNote(note) {
+        const element = document.createElement('div');
+        element.innerHTML = note;
+        element.className = 'note';
+        content.appendChild(element);
+    }
+    
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+        var note = input.value;
+        input.value = '';
+        addNote(note);
+    });
+    
+});
